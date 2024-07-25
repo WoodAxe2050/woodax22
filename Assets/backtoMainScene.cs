0b2cf5f6bf4e1a25c7b9943cd1754d6e42e8638b
WoodAxe2050 <163821721+WoodAxe2050@users.noreply.github.com>
2024-07-25 19:33:15 +0900

    새 파일 생성
    첫 시작

---
+++ Assets/backtoMainScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backtoMainScene : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        // "Inventory" 씬으로 전환합니다.
+        SceneManager.LoadScene("Mainscene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
